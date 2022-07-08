affd40ad4d1c410e76228e67be8be0e6d373df55
unknown <sebastianswiecz458@gmail.com>
2022-07-08 21:59:08 +0200

    Added check if product exists for product page

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { Data } from 'src/Data';
+import { formatValue } from 'utils/formatValue';
 
 function Porduct() {
   const router = useRouter();
@@ -7,12 +9,19 @@ function Porduct() {
   const productTitle = typeof slug === 'string' && slug.split(':')[0];
   const productID = typeof slug === 'string' && slug.split(':')[1];
 
-  return (
-    <main>
-      <h2>Product: {productTitle}</h2>
-      <p>ID: {productID}</p>
-    </main>
-  );
+  const productExist = Data.find((item) => item.id === +productID);
+
+  if (productExist)
+    return (
+      <main>
+        <h1>Produkt: {productTitle}</h1>
+        <p>ID: {productID}</p>
+
+        <p>Cena: {formatValue(productExist.price)}</p>
+        <p>Opis: {productExist.description}</p>
+      </main>
+    );
+  else return <h1>Produkt nie został znaleziony.</h1>;
 }
 
 export default Porduct;
